eb46727d1317413ce7254686238994ee164009c3
DjairCosta <riajd10@gmail.com>
2022-01-20 05:53:18 -0300

    first commit

---
+++ src/test/java/com/daytech/aws_project/AwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daytech.aws_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
